65b690185053cdf78264c3c22b9bdd2c6b43cf73
Paul Göckede <pgoeckede@googlemail.com>
2022-01-10 23:55:26 +0100

    Added the GUI and removed the rule that you have to play 5 rounds

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
+
 Odin-Project: First JS project, simple rock, paper, scissors. For now only through console.
+
+10.01.2022
+
+Added the GUI
--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let userScore = 0;
 let compScore = 0;
 
-// Create a function where a random output either rock paper or scissor is generated
-// this will be the computers pick
+const topDiv = document.querySelector(".buttons");
+const bottomDiv = document.querySelector(".results");
+const scoreDiv = document.querySelector(".scores");
+const compScoreDiv = document.querySelector(".computerScore");
+const userScoreDiv = document.querySelector(".userScore");
 
-function computer(){
-    let ranNum = Math.floor(Math.random() * 3); //random num between 0 and 2
+//This function randomly returns either "ROCK", "PAPER" or "SCISSORS" as a pick
+
+function computer(){  
+    let ranNum = Math.floor(Math.random() * 3);
     let pick;
 
-    //Assigns either Rock Paper or Scissors as the computers pick based on the random number
     switch(ranNum){
         case 0:
             pick = "ROCK";
@@ -26,88 +30,144 @@ function computer(){
     return pick;
 }
 
-// create a function where the game is played with two parameters, your pick and the computers pick
+//This function plays one round of Rock Paper Scissors with the users Pick being Rock
 
-function play(comp){
-    let userPick = prompt("Pick either Rock, Paper or Scissors", "");
+function playRock(){
+    const compPick = computer();
 
-    //make the pick case insensitive
-    userPick = userPick.toUpperCase();
+    const resultMessage = document.createElement("h1");
+    resultMessage.classList.add("header");
 
-    //game
-    switch(userPick){
-        case "ROCK": //options if user picked rock
-            switch(comp){
-                case "ROCK":
-                    console.log("Draw! Computer also picked Rock.");
-                    break;
-                case "PAPER":
-                    console.log("You lost! Computer picked Paper.");
-                    compScore++;
-                    break;
-                case "SCISSORS":
-                    console.log("You win! Computer picked scissors.");
-                    userScore++;
-                    break;
-            }
+    const compScoreText = document.querySelector(".compScoreText");
+    compScoreText.textContent = "Computer Score:";
+    const compScoreNumber = document.querySelector(".compScoreNumber");
+
+    const userScoreText = document.querySelector(".userScoreText");
+    userScoreText.textContent = "Your Score:";
+    const userScoreNumber = document.querySelector(".userScoreNumber");
+
+    switch(compPick){
+        case "ROCK":
+            resultMessage.classList.add("draw");
+            resultMessage.textContent = "Draw! Computer also picked Rock.";
             break;
-        case "PAPER": //options if user picked paper
-            switch(comp){
-                case "ROCK":
-                    console.log("You win! Computer picked Rock.");
-                    userScore++;
-                    break;
-                case "PAPER":
-                    console.log("Draw! Computer also picked paper.");
-                    break;
-                case "SCISSORS":
-                    console.log("You lost! Computer picked scissors.");
-                    compScore++;
-                    break;
-            }
+        case "PAPER":
+            resultMessage.classList.add("lost");
+            resultMessage.textContent = "You lost! Computer picked Paper.";
+            compScore++;
             break;
-        case "SCISSORS": //options if user picked scissors
-            switch(comp){
-                case "ROCK":
-                    console.log("You lost! Computer picked Rock.");
-                    compScore++;
-                    break;
-                case "PAPER":
-                    console.log("You win! Computer  picked paper.");
-                    userScore++;
-                    break;
-                case "SCISSORS":
-                    console.log("Draw! Computer also picked scissors.");
-                    break;
-            }
-            break; 
-        default: //fallback if user picked none of the three
-            console.log("Pick either Rock, Paper or Scissors!");
+        case "SCISSORS":
+            resultMessage.classList.add("won");
+            resultMessage.textContent = "You win! Computer picked scissors.";
+            userScore++;
             break;
     }
 
-    console.log("Computer Score: " + compScore);
-    console.log("Your Score: " + userScore);
+    bottomDiv.removeChild(document.querySelector(".header"));
+    bottomDiv.insertBefore(resultMessage, scoreDiv);
+
+    compScoreNumber.textContent = compScore;
+    userScoreNumber.textContent = userScore;
+}
+
+//This function plays one round of Rock Paper Scissors with the users Pick being Paper
+
+function playPaper(){
+    const compPick = computer();
+
+    const resultMessage = document.createElement("h1");
+    resultMessage.classList.add("header");
+
+    const compScoreText = document.querySelector(".compScoreText");
+    compScoreText.textContent = "Computer Score:";
+    const compScoreNumber = document.querySelector(".compScoreNumber");
+
+    const userScoreText = document.querySelector(".userScoreText");
+    userScoreText.textContent = "Your Score:";
+    const userScoreNumber = document.querySelector(".userScoreNumber");
+
+    switch(compPick){
+        case "ROCK":
+            resultMessage.classList.add("won");
+            resultMessage.textContent = "You won! Computer picked Rock.";
+            userScore++;
+            break;
+        case "PAPER":
+            resultMessage.classList.add("draw");
+            resultMessage.textContent = "Draw! Computer also picked Paper.";
+            break;
+        case "SCISSORS":
+            resultMessage.classList.add("lost");
+            resultMessage.textContent = "You lost! Computer picked scissors.";
+            compScore++;
+            break;
+    }
+
+    bottomDiv.removeChild(document.querySelector(".header"));
+    bottomDiv.insertBefore(resultMessage, scoreDiv);
+
+    compScoreNumber.textContent = compScore;
+    userScoreNumber.textContent = userScore;
     
 }
 
-function game(){
-    userScore = 0;
-    compScore = 0;
+//This function plays one round of Rock Paper Scissors with the users Pick being Scissors
 
-    for(let i=0; i<5; i++){
-        play(computer());
+function playScissors(){
+    const compPick = computer();
+
+    const resultMessage = document.createElement("h1");
+    resultMessage.classList.add("header");
+
+    const compScoreText = document.querySelector(".compScoreText");
+    compScoreText.textContent = "Computer Score:";
+    const compScoreNumber = document.querySelector(".compScoreNumber");
+
+    const userScoreText = document.querySelector(".userScoreText");
+    userScoreText.textContent = "Your Score:";
+    const userScoreNumber = document.querySelector(".userScoreNumber");
+
+    switch(compPick){
+        case "ROCK":
+            resultMessage.classList.add("lost");
+            resultMessage.textContent = "You lost! Computer picked Rock.";
+            compScore++;
+            break;
+        case "PAPER":
+            resultMessage.classList.add("won");
+            resultMessage.textContent = "You won! Computer picked Paper.";
+            userScore++;
+            break;
+        case "SCISSORS":
+            resultMessage.classList.add("draw");
+            resultMessage.textContent = "Draw! Computer also picked scissors.";
+            break;
     }
 
-    if(userScore>compScore){
-        console.log("You won! Congratulations.");
-    } else if(compScore > userScore){
-        console.log("Oh no! You lost. Better luck next time.");
-    } else{
-        console.log("It's a draw. Maybe play again.");
-    }
+    bottomDiv.removeChild(document.querySelector(".header"));
+    bottomDiv.insertBefore(resultMessage, scoreDiv);
+
+    compScoreNumber.textContent = compScore;
+    userScoreNumber.textContent = userScore;
+    
 }
 
-document.getElementById("test").addEventListener("click", () => game());
+function checkScore(computerScore, userScoreCheck){
+    if(computerScore===5 || userScoreCheck===5){
+        return true;
+    }
+    return false;
+}
+
+const buttonRock = document.querySelector("#rock");
+buttonRock.addEventListener('click', playRock);
+
+const buttonPaper = document.querySelector("#paper");
+buttonPaper.addEventListener("click", playPaper);
+
+const buttonScissors = document.querySelector("#scissors");
+buttonScissors.addEventListener("click", playScissors);
+
+
 
 // create a new function game() and use the previous function to play 5 rounds of RPS. Function keeps score and declares winner at the end
